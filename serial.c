404d4c545228778ba7b927fd8155be5c85846543
Chris Roscher <chris@aitai.nl>
2018-10-19 14:46:16 +0200

    Paar kleine aanpassingen

--- serial.c
+++ serial.c
@@ -1,13 +1,13 @@
 void uart_init(){
-UBRR0H = 0;
-UBRR0L = UBBRVAL;
-UCSR0B |= _BV(RXEN0) | _BV(TXEN0);
-UCSR0C |= _BV(UCSZ00) | _BV(UCSZ01);
+	UBRR0H = 0;
+	UBRR0L = UBBRVAL;
+	UCSR0B |= _BV(RXEN0) | _BV(TXEN0);
+	UCSR0C |= _BV(UCSZ00) | _BV(UCSZ01);
 }
 
 void tx(uint8_t data) {
-loop_until_bit_is_set(UCSR0A, UDRE0);
-UDR0 = data;
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	UDR0 = data;
 }
 
 void txChar(char message[]) {
@@ -24,6 +24,6 @@ void txChar(char message[]) {
 }
 
 uint8_t rx() {
-loop_until_bit_is_set(UCSR0A, RXC0);
-return UDR0;
+	loop_until_bit_is_set(UCSR0A, RXC0);
+	return UDR0;
 }

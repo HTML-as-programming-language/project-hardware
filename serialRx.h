43abaf2436dde720b3c4cc29cdfe220a13be9670
Chris Roscher <chris@aitai.nl>
2018-10-27 01:56:29 +0200

    Opmaak aangepast, uart_init terug in ttc.c gezet en sendPacket gebruiken voor alle functies.

--- serialRx.h
+++ serialRx.h
@@ -1,17 +1,49 @@
 union
 {
+	uint32_t IntVar;
+	unsigned char Bytes[4];
+} lastMessage;
+
+union
+{
 	uint16_t IntVar;
 	unsigned char Bytes[2];
-}
-firstInt;
+} firstInt;
 
-checkRx()
+unsigned char rx( void )
+{
+	while ( !(UCSR0A & (1<<RXC0)) );
+	return UDR0;
+}
+
+void handleRx()
+{
+	//leest lastMessage en neemt de bijbehorede acties
+	int command = ((lastMessage.Bytes[0] * 0x100) + lastMessage.Bytes[1]); //het commando (bovenste 16 bits)
+	int payload = ((lastMessage.Bytes[2] * 0x100) + lastMessage.Bytes[3]); //de payload van het bericht
+	switch(command)
+	{
+		case 11:
+			// tempOn = payload;
+			break;
+		case 12:
+			// tempOff = payload;
+			break;
+		case 51:
+			// setScreen(0xff);
+			break;
+		case 52:
+			// setScreen(0x00);
+			break;
+	}
+}
+
+void checkRx()
 { //checkt of er een bericht is binnengekomen op rx en schrijft het naar een variabele
 	firstInt.Bytes[0] = rx(); //schrijft het bericht naar de bovenste helft van een int
-	firstInt.Bytes[1] = rx(); //alle berichten bestaan uit 16 bits, hier word de tweede helft geschreven
+	firstInt.Bytes[1] = rx(); //alle berichten bestaan uit 16 bits, hier wordt de tweede helft geschreven
 	if (firstInt.IntVar == 0xffff)
-	{ //0xffff betekend dat het het begin is van een bericht is, de rest van het bericht wordt nu naar een variabele geschreven
-		tx(0x11);
+	{ //0xffff betekent dat het het begin is van een bericht is, de rest van het bericht wordt nu naar een variabele geschreven
 		lastMessage.Bytes[0] = rx();
 		lastMessage.Bytes[1] = rx();
 		lastMessage.Bytes[2] = rx();
@@ -19,24 +51,3 @@ checkRx()
 		handleRx();
 	}
 }
-
-void handleRx()
-{
-          //leest lastMessage en neemt de bijbehorede acties 
-          int command = ((lastMessage.Bytes[0] * 0x100) + lastMessage.Bytes[1]); //het commando (bovenste 16 bits
-          int payload = ((lastMessage.Bytes[2] * 0x100) + lastMessage.Bytes[3]); //de payload van het bericht
-          switch(command)
-          {
-                  case 11: 
-                          tempOn = payload;
-                          break; 
-                  case 12: 
-                          tempOff = payload;
-                          break;
-                  case 51:  
-                          setScreen(0xff);
-                          break;  
-                  case 52:  
-                          setScreen(0x00);
-        }
- }
--- serialTx.h
+++ serialTx.h
@@ -1,11 +1,14 @@
-union {uint32_t IntVar; unsigned char Bytes[4];} lastMessage;
+union
+{
+	unsigned char Bytes[4];
+	uint16_t Ints[2];
+} message;
 
-void uart_init(){
-	UBRR0H = 0;
-	UBRR0L = UBBRVAL;
-	UCSR0B |= _BV(RXEN0) | _BV(TXEN0);
-	UCSR0C |= _BV(UCSZ00) | _BV(UCSZ01);
-}
+union
+{
+	uint16_t ValInt;
+	unsigned char Bytes[2];
+} MessInt;
 
 void tx( unsigned char data )
 {
@@ -13,29 +16,13 @@ void tx( unsigned char data )
 	UDR0 = data;
 }
 
-void txChar(uint8_t message[]) {
+void txChar(uint8_t message[])
+{
 	for (int c = 0; message[c] != 0; c++)
+	{
 		tx(message[c]);
+	}
 
-}
-
-void txInt(int message) {
- 	//verzend een int (16 bit) in twee packets van 8 bits
-	union {uint16_t ValInt; unsigned char Bytes[2];} MessInt;
-	MessInt.ValInt = message;
- 	tx(MessInt.Bytes[1]);
-	tx(MessInt.Bytes[0]);
-}
-
-unsigned char rx( void )
-{
-	while ( !(UCSR0A & (1<<RXC0)) );
-	return UDR0;
-}
-
-void sendString()
-{
-	txChar("Hello World");
 }
 
 void startPacket()
@@ -44,8 +31,8 @@ void startPacket()
 	tx(0xff);
 }
 
-void sendPacket(int command, int payload) {
-	union {uint32_t IntVar; unsigned char Bytes[4]; uint16_t Ints[2];} message;
+void sendPacket(int command, int payload)
+{
 	message.Ints[1] = command;
 	message.Ints[0] = payload;
 	startPacket();

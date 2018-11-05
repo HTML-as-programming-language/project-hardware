778f056e65195f4ac0413e2fb7c1e2614373389a
Simon Wielenga <s.wielenga@live.nl>
2018-11-05 14:25:25 +0100

    Added comments

--- servo.h
+++ servo.h
@@ -5,7 +5,7 @@ void servo_init() {
 	DDRD |= 0x08; // set pin 3 als output
 	TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
   	TCCR2B = _BV(WGM22) | _BV(CS20);
-  	OCR2A = 63;
+  	OCR2A = 0xFF;
 	OCR2B = 0x00; //000
 }
 

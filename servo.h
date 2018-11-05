7413beff6abc47aed08d84ae8bd7e8428211203f
Simon Wielenga <s.wielenga@live.nl>
2018-11-05 14:27:35 +0100

    Added comments (this time for real)

--- servo.h
+++ servo.h
@@ -5,8 +5,8 @@ void servo_init() {
 	DDRD |= 0x08; // set pin 3 als output
 	TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
   	TCCR2B = _BV(WGM22) | _BV(CS20);
-  	OCR2A = 0xFF;
-	OCR2B = 0x00; //000
+  	OCR2A = 0xFF; // set de maximale waarde voor de counter van de pwm
+	OCR2B = 0x00; //set de duty cycle
 }
 
 void servo_set(uint8_t value) { //value is een getal tussen de 0x00 en 0xff
